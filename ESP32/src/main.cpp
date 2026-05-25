13906778b4bae99058414a9a8b6ba8ecdd4c9db8
abbyfinka <abbyfinka1@gmail.com>
2026-05-25 16:32:33 +0100

    eight filters

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -44,7 +44,7 @@ float input[N_FEATURES];
 BiQuadFilterDF1<float> biquad_notch_filter[8];
 BiQuadFilterDF1<float> biquad_hp_filter[8];
 BiQuadFilterDF1<float> biquad_lp_filter[8];
-EMA<EMA_K, float> ema;
+EMA<EMA_K, float> ema[8];
 
 EMGData currentWindow[WINDOW_SIZE] = {0};
 EMGData nextWindow[WINDOW_SIZE] = {0};
@@ -130,7 +130,7 @@ void parseADSDataTask(void * pvParameters)
         if (BANDPASS_EN && !TEST_DATA_EN)
         {
           // Exponential Moving Average filter to remove fluctuating DC offset
-          convertedData = ema.filter(convertedData);
+          convertedData = ema[i].filter(convertedData);
 
           // applying biquad filters
           convertedData = biquad_hp_filter[i](convertedData); // applying highpass filter
@@ -297,10 +297,11 @@ void setup()
       // initialising high pass and low pass filters with calculated coefficients
       for (int i = 0; i < 8; i++)
       {
+        ema[i] = EMA<EMA_K, float>();
         biquad_hp_filter[i] = BiQuadFilterDF1<float>({h_b0, h_b1, h_b2}, {h_a0, h_a1, h_a2});
         biquad_lp_filter[i] = BiQuadFilterDF1<float>({l_b0, l_b1, l_b2}, {l_a0, l_a1, l_a2});
       }
-      ema = EMA<EMA_K, float>();
+      
     }
   }
 
